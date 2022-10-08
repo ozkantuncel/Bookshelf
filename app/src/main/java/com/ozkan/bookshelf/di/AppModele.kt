0b105fccadbec170edcf8c2dea0820a8b08dcae7
Ozkan Tuncel <ozkantuncel2016@gmail.com>
2022-10-08 13:14:31 +0300

    Add dagger hilt and firebase and setup it

---
+++ app/src/main/java/com/ozkan/bookshelf/di/AppModele.kt
@@ -0,0 +1,12 @@
+package com.ozkan.bookshelf.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModele {
+
+}
---
+++ app/src/main/java/com/ozkan/bookshelf/di/BookshelfApp.kt
@@ -0,0 +1,7 @@
+package com.ozkan.bookshelf.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BookshelfApp:Application()
